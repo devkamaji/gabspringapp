1dd28357c917a701eac1b49293a7a684997726a9
Gabriel Nogueira <gabriel.nogueira21@hotmail.com>
2025-03-23 00:43:14 -0300

    Add initial project structure and configuration files

---
+++ src/main/java/br/gabspringapp/GabSpringApp.java
@@ -0,0 +1,9 @@
+package br.gabspringapp;
+
+import br.com.gabspring.web.GabSpringApplication;
+
+public class GabSpringApp {
+    public static void main(String[] args) {
+        GabSpringApplication.run(GabSpringApp.class);
+    }
+}
